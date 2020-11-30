2fe3d4e71b2cf029e946808c4c301d58f55a4be3
Pierre-Yves GUENNOC <pguennoc@enssat.fr>
2020-11-30 09:44:39 +0100

    js-sw

--- js/pwa/serviceworker.js
+++ js/pwa/serviceworker.js
@@ -3,18 +3,15 @@ var staticCacheName = "serviceWorker";
 var cors_api_url = 'https://cors-anywhere.herokuapp.com/';
 var url = 'https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/9EYlGR3a.shu'
 
-var filesToCache = [
-    '/js/pwa/',
-    'https://cors-anywhere.herokuapp.com/https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/9EYlGR3a.shu'
-]
-
-
-self.addEventListener("install", function (e) {
-    e.waitUntil(
-        caches.open(staticCacheName).then(function (cache) {
-            return cache.addAll(filesToCache);
-        })
-    );
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/js/pwa/',
+        'https://cors-anywhere.herokuapp.com/https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/9EYlGR3a.shu'
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', function (event) {
